07c135b0083762863902bcf4f4c47713e8ab4fc8
Abdul <23074754+crabdul@users.noreply.github.com>
2018-02-13 14:50:28 +0000

    initial commit

---
+++ components/home/BlockTiles.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul class="post-list">
+    <li v-for="post in posts" :key="post.id" class="post-list-item">
+      <BlockTile :post="post" />
+    </li>
+  </ul>
+</template>
+
+<script>
+import BlockTile from './BlockTile.vue'
+
+export default {
+  props: {
+    posts: { type: Array }
+  },
+  components: {
+    BlockTile
+  }
+}
+</script>
+
+<style scoped>
+.post-list {
+  padding: 0;
+}
+
+.post-list-item {
+  list-style: none;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="container">
+    <div>
+      <BlockTiles :posts="posts" />
+    </div>
+  </section>
+</template>
+
+<script>
+import BlockTiles from '~/components/home/BlockTiles.vue'
+import wp from '~/lib/wp'
+
+export default {
+  data() {
+    return {
+      posts: [],
+    }
+  },
+  async asyncData ({ params }) {
+    return wp.posts()
+  },
+  components: {
+    BlockTiles
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 1rem auto;
+  width: 90%;
+  max-width: 900px;
+}
+</style>
