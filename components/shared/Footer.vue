1c132fc613502ddbf90805b6e440ce45665a4690
Abdul <23074754+crabdul@users.noreply.github.com>
2018-02-13 18:47:36 +0000

    Footer and header added sans font styling

---
+++ components/shared/Footer.vue
@@ -0,0 +1,40 @@
+<template>
+  <footer>
+      <div class="footer__container">
+            <div class="footer__logo">
+                Karai Music © 2018
+            </div>
+            <div class="footer__info">
+                Designed by <a href="https://www.linkedin.com/in/el-khazaani/" target="_blank">Karim El Khazaani</a> Hosted By <a href="https://www.digitalocean.com/" target="_blank">Digital Ocean</a>
+            </div>
+      </div>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+    footer {
+        display: grid;
+        font-family: 'Lato', sans-serif;
+        color: white;
+        height: 10vh;
+        background-color: #181818;
+        text-align: center;
+        font-size: 10pt;
+
+        .footer__container {
+            margin: auto;
+
+            * {
+                margin: 4px 0;
+            }
+        }
+
+        a {
+            text-decoration: none;
+            color: white;
+            &:hover {
+                color: #E91E63;
+            }
+        }
+    }
+</style>
