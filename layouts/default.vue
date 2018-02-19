f91dc17813d0ccc008b8bd691f85b73da70c1a4e
Abdul <23074754+crabdul@users.noreply.github.com>
2018-02-19 11:33:59 +0000

    all posts showing on home / radio tiles added

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hello">
+  <div class="">
     <app-header/>
     <nuxt/>
     <appFooter/>
@@ -34,4 +34,42 @@ export default {
 
 
 <style>
+body {
+    background-color: #111111;
+}
+.spacer {
+  min-height:  92vh;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: all .3s ease-out;
+}
+.fade-enter {
+  transform: translate3d(-96px,0,0);
+  opacity: 0;
+}
+.fade-enter-to {
+  transform: translate3d(0,0,0);
+  opacity: 1;
+}
+.fade-leave {
+  transform: translate3d(0,0,0);
+  opacity: 1;
+}
+.fade-leave-to {
+  transform: translate3d(96px,0,0);
+  opacity: 0;
+}
+
+.slide-enter-active, 
+.slide-leave-active {
+  transition: all 0.3s ease-in-out;
+}
+.slide-enter {
+  transform: translate3d(0,96px,0);
+}
+.slide-leave-to {
+  transform: translate3d(0,96px,0);
+}
 </style>
