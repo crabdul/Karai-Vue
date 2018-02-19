47663997a01d3d1eb9f16417803b494e57f4a34a
Abdul <23074754+crabdul@users.noreply.github.com>
2018-02-19 16:08:15 +0000

    posts is array

--- pages/tracks/_track/index.vue
+++ pages/tracks/_track/index.vue
@@ -1,38 +1,34 @@
 <template>
     <div id="posts">
-        <!-- <ul> -->
-            <!-- <li v-for="(post, index) in posts" :key="index"> -->
-                <div class="post" :id="'post__' + index">
-                    <div class="post-details__container">
-                        <div class="post-details">
-                            <h3>{{ post.acf.title }} </h3>
-                            <h3 class="post-details__artist">{{ post.acf.artist }} </h3>
-                            <div class="post-details__meta">
-                                <h4 class="post-details__author">BY: Jolyon </h4>
-                                <h4 class="post-details__date">{{ post.acf.date }} </h4>
-                            </div>
-                        </div>
-                        <div class="post-youtube">
-                            <div class="post-youtube__play-button" v-on:click="playSong({title:post.field_title[0].value, artist:post.field_artist[0].value, image:post.field_cover_art[0].url, videoId:post.field_videoid[0].value})">
-                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="isolation:isolate" viewBox="0 0 64 64" width="64" height="64"><defs><clipPath id="_clipPath_eOto5QOSpvHxKbZ0mZKtvhDpJgb6C9rR"><rect width="64" height="64"/></clipPath></defs><g clip-path="url(#_clipPath_eOto5QOSpvHxKbZ0mZKtvhDpJgb6C9rR)"><path d=" M 1 32 C 1 14.891 14.891 1 32 1 C 49.109 1 63 14.891 63 32 C 63 49.109 49.109 63 32 63 C 14.891 63 1 49.109 1 32 Z " fill="none" vector-effect="non-scaling-stroke" stroke-width="1.5" stroke="rgb(255,255,255)" stroke-linejoin="miter" stroke-linecap="square" stroke-miterlimit="3"/><polygon points="44,32.5,25,47,25,18" fill="rgb(255,255,255)"/></g></svg>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="post-image__container">
-                        <div class="post-image">
-                            <img :src="post.acf.cover_art" :alt="`${post.acf.title} ${post.acf.artist}`">
-                        </div>
-                        <div class="post-image__copyright">
-                            {{ post.acf.label }}
-                        </div>
-                    </div>
-                    <div class="post-content__container">
-                        <div class="post-content" v-html="post.content.rendered">
-                        </div>
+        <div class="post" :id="'post__'+index" v-for="(post, index) in posts" :key="index">
+            <div class="post-details__container">
+                <div class="post-details">
+                    <h3>{{ post.acf.title }} </h3>
+                    <h3 class="post-details__artist">{{ post.acf.artist }} </h3>
+                    <div class="post-details__meta">
+                        <h4 class="post-details__author">BY: Jolyon </h4>
+                        <h4 class="post-details__date">{{ post.acf.date }} </h4>
                     </div>
                 </div>
-            <!-- </li> -->
-        <!-- </ul> -->
+                <div class="post-youtube">
+                    <div class="post-youtube__play-button" v-on:click="playSong({title:post.field_title[0].value, artist:post.field_artist[0].value, image:post.field_cover_art[0].url, videoId:post.field_videoid[0].value})">
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="isolation:isolate" viewBox="0 0 64 64" width="64" height="64"><defs><clipPath id="_clipPath_eOto5QOSpvHxKbZ0mZKtvhDpJgb6C9rR"><rect width="64" height="64"/></clipPath></defs><g clip-path="url(#_clipPath_eOto5QOSpvHxKbZ0mZKtvhDpJgb6C9rR)"><path d=" M 1 32 C 1 14.891 14.891 1 32 1 C 49.109 1 63 14.891 63 32 C 63 49.109 49.109 63 32 63 C 14.891 63 1 49.109 1 32 Z " fill="none" vector-effect="non-scaling-stroke" stroke-width="1.5" stroke="rgb(255,255,255)" stroke-linejoin="miter" stroke-linecap="square" stroke-miterlimit="3"/><polygon points="44,32.5,25,47,25,18" fill="rgb(255,255,255)"/></g></svg>
+                    </div>
+                </div>
+            </div>
+            <div class="post-image__container">
+                <div class="post-image">
+                    <img :src="post.acf.cover_art" :alt="`${post.acf.title} ${post.acf.artist}`">
+                </div>
+                <div class="post-image__copyright">
+                    {{ post.acf.label }}
+                </div>
+            </div>
+            <div class="post-content__container">
+                <div class="post-content" v-html="post.content.rendered">
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -64,18 +60,15 @@ import wp from '~/lib/wp'
         // },
         data () {
             return {
-                post: ''
+                posts: []
             }
         },
 
         created () {
-            this.post = this.$store.getters.getTrackBySlug(this.$route.path)
+            this.posts.push(this.$store.getters.getTrackBySlug(this.$route.path))
         },
 
         methods: {
-            getDate: function(date) {
-                return new Date(date).toString().split(' ').splice(1,3).join(' ');
-            },
             // Determine if central element is in the viewport
             isInViewportSingle: function(element) {
                 var rect = element.getBoundingClientRect();
