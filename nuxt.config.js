12a5c2305f8cf271274adc6149e4a735fd1718af
Abdul <23074754+crabdul@users.noreply.github.com>
2018-02-21 23:08:44 +0000

    update hid meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'karai music front-end' }
+      { hid: 'description', name: 'description', content: 'Karai Music - Music Sharing Site' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
