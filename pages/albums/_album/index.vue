e41d327501d00de6d1d176d1308ef74765b6cfed
Abdul <23074754+crabdul@users.noreply.github.com>
2018-03-20 22:13:17 +0000

    album head tags

--- pages/albums/_album/index.vue
+++ pages/albums/_album/index.vue
@@ -16,6 +16,20 @@ import InfinityContainer from '~/components/post/InfinityContainer.vue'
             return {
                 post
             }
+        },
+        head () {
+            return {
+                title: `${this.post[0].acf.title} - ${this.post[0].acf.artist} | Karai Music`,
+                meta: [
+                    { hid: 'description', name: 'description', content: this.post[0].acf.abstract },
+                    { hid: 'og:description', property: 'og:description', content: this.post[0].acf.abstract },
+                    { property: 'og:type', content: 'article' },
+                    { hid: 'og:title', property: 'og:title', content: `${this.post[0].acf.title} - ${this.post[0].acf.artist} | Karai Music`},
+                    { property: 'og:image', content: this.post[0].acf.cover_art },
+                    { property: 'og:image:alt', content: `${this.post[0].acf.title} - ${this.post[0].acf.artist}` },
+                    { hid: 'og:url', property: 'og:url', content: `https://karaimusic.com/albums/${this.post[0].slug}`},
+                ]
+            }
         }
     }
     
